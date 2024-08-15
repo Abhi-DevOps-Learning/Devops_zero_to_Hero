5d78db64c6d12063e737bab17d2f9b69aeb08a47
Abhishek-Sontakke <sontakkeabhishek27@gmail.com>
2024-08-15 21:01:08 +0530

    Add files via upload

---
+++ ifelse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#########demo ifelse script##################
+
+a=10
+b=20
+
+if [ $b -gt $a ]; then	
+   echo "$b is greater than $a"
+else 
+    echo "$a is less than or equlas to $b"
+fi	
